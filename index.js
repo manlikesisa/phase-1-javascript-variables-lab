1a1c7644602a952e911b0a2ee7b9216f4010f4ff
clinton sisa <clinton.sisa@student.moringaschool.com>
2025-08-20 15:42:18 +0300

    completed the variable lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Code your solution in this file!
+// // Code your solution in this file!
+let companyName = 'Scuber';
+// // const companyName = 'scuber'
+let mostProfitableNeighborhood ='Chelsea';
+let companyCeo = 'Susan Smith';
+// companyCeo = 'susan smith';
